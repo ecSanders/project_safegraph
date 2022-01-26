7aceec26b1b0b216e0955bd2c5972fb342bdefc8
ecSanders <ecsander0@gmail.com>
2022-01-26 14:20:44 -0700

    edits

--- graphql_noauth.py
+++ graphql_noauth.py
@@ -8,14 +8,25 @@ query = """
 query {
     characters {
     results {
+      gender
       name
       status
       species
       type
-      gender
+      image
+      origin {
+        dimension
+        created
+      }
+      episode {
+        episode
+        name
+        air_date
+      }
     }
   }
 }
+
 """
 # %%
 url = 'https://rickandmortyapi.com/graphql/'
@@ -23,8 +34,10 @@ r = requests.post(url, json={'query': query})
 print(r.status_code)
 print(r.text)
 # %%
-json_data = json.loads(r.text)
+json_data = json.loads(r.text) # b\c its a string
 # %%
 df_data = json_data['data']['characters']['results']
 df = pd.DataFrame(df_data)
+# %%
+df
 # %%
